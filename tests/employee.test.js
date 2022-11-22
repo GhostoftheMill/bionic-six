42560fa4b522987bda877f5fdce36f9db1be60e4
GhostoftheMill <kevinmichaelwhite@gmail.com>
2022-11-22 15:37:40 -0500

    Added objectives to class files in lib

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -6,3 +6,12 @@
 //getId()
 //getEmail()
 //getRole() returns Employee
+
+//reference file to test
+const employee = require('../lib/employee');
+
+test('name is string', function() {
+    var employee = Employee();
+    expect(employee.name).toEqual(expect.any(String));
+    
+})
