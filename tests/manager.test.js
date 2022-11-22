4c1ee918081729efc921c2fd52d6f06df6ec0817
GhostoftheMill <kevinmichaelwhite@gmail.com>
2022-11-22 18:38:35 -0500

    Created manager test

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,3 +1,14 @@
 //things to test
+const manager = require('../lib/manager');
+
 //officeNumber
+test('manager', function() {
+    var manager = manager();
+        expect(manager.number).toEqual(expect.any(value));
+    });
+
 //getRole() // Override return Manager
+test('role', function() {
+    var manager = manager();
+        expect(manager.getRole()).toExpect("Manager");
+    });
